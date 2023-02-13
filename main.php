319e83686394bf4dff65192e5f2eec912f5029e4
NeitKait <tunhanhnhau@gmail.com>
2023-02-14 04:17:42 +0700

    tat ca tai tuan anh kcsc task

--- main.php
+++ main.php
@@ -15,11 +15,13 @@ require 'config.php';
         $username = $_GET['username'];
         $sql = "SELECT * FROM member WHERE username = '$username'";
         $result = mysqli_query($conn,$sql);
-        $row= mysqli_fetch_assoc(mysqli_query($conn,$sql));      
+        $row= mysqli_fetch_assoc(mysqli_query($conn,$sql)); 
+        echo 'username: ';
         echo $row['username'];
         echo '<br>';
         echo 'password: ';
-        echo $row['password'];        
+        echo $row['password']; 
+        echo '<br>';       
     ?>
     <a href="logout.php">dang xuat</a>
     <br>
